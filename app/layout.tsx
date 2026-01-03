6c1a6384152f05ff4335552930b03df75b5aa06c
hyein89 <bihontai@gmail.com>
2026-01-04 03:58:54 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,13 @@
+
 import "./globals.css";
 
-
-export const metadata = {
-  title: "Watch Video - 060",
-  description:
-    "Just Pron Tv - Watch free full videos on traxxxcr07",
-  icons: {
-    icon: "https://i.ibb.co.com/Y7q3Bv3K/5988576.png",
-    apple: "https://i.ibb.co.com/Y7q3Bv3K/5988576.png"
-  },
-  openGraph: {
-    images: [
-      "https://i.ibb.co.com/TxpdHXJC/12604576-12604576-880x660.jpg"
-    ]
-  }
-};
-
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html>
       <body>{children}</body>
     </html>
   );
