74e909377052f37625150614555e8fdffee06806
hyein89 <bihontai@gmail.com>
2026-01-04 05:19:56 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,15 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Create Redirect URL",
-  description: "Create safe redirect URLs with tracking and fallback system",
   icons: {
     icon: "/favicon.png",
     apple: "/favicon.png",
   },
   openGraph: {
     title: "Create Redirect URL",
-    description: "Create safe redirect URLs with tracking and fallback system",
     images: [
       {
         url: "/og-default.jpg",
@@ -29,7 +28,39 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        {/* HISTATS GLOBAL */}
+        <Script
+          id="histats"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _Hasync = _Hasync || [];
+              _Hasync.push(['Histats.start', '1,4828760,4,0,0,0,00010000']);
+              _Hasync.push(['Histats.fasi', '1']);
+              _Hasync.push(['Histats.track_hits', '']);
+              (function() {
+                var hs = document.createElement('script');
+                hs.type = 'text/javascript';
+                hs.async = true;
+                hs.src = '//s10.histats.com/js15_as.js';
+                (document.head || document.body).appendChild(hs);
+              })();
+            `,
+          }}
+        />
+      </head>
+
+      <body>
+        {children}
+
+        {/* HISTATS NOSCRIPT */}
+        <noscript>
+          <a href="/" target="_blank" rel="noreferrer">
+            <img src="//sstatic1.histats.com/0.gif?4828760&101" alt="" />
+          </a>
+        </noscript>
+      </body>
     </html>
   );
 }
