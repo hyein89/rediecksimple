e7401e12a1de4fc55724096a1d7ee99924d9a604
hyein89 <bihontai@gmail.com>
2026-01-04 04:01:41 +0700

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,51 +1,16 @@
-"use client";
 
-import links from "../../data/links.json";
+import links from "@/data/links.json";
 import { notFound } from "next/navigation";
-import { useEffect } from "react";
+import RedirectLogic from "@/components/RedirectLogic";
 
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string };
-};
-
-export default function RedirectPage({
+export default function Page({
   params,
-  searchParams
-}: Props) {
-  const data = links.find((x) => x.id === params.id);
+}: {
+  params: { id: string };
+}) {
+  const valid = links.find((x) => x.id === params.id);
 
-  // 🔴 ID tidak ada → 404
-  if (!data) notFound();
+  if (!valid) notFound();
 
-  const targetUrl = Object.keys(searchParams)[0];
-
-  useEffect(() => {
-    if (targetUrl) {
-      setTimeout(() => {
-        window.location.href = targetUrl;
-      }, 2000);
-    }
-  }, [targetUrl]);
-
-  return (
-    <div id="redirect">
-      <p id="redirect-testing">
-        Testing provided URLs...
-      </p>
-
-      {targetUrl && (
-        <p id="redirect-found">
-          Redirecting you to{" "}
-          <a href={targetUrl}>{targetUrl}</a>
-        </p>
-      )}
-
-      {!targetUrl && (
-        <p id="redirect-none">
-          None of the provided URLs work!
-        </p>
-      )}
-    </div>
-  );
+  return <RedirectLogic />;
 }
