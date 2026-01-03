c5bb8b5e419f27a535869415b774576d8220ff28
hyein89 <bihontai@gmail.com>
2026-01-04 04:43:48 +0700

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div style={{ textAlign: "center", padding: 50 }}>
       <h1>404</h1>
-      <p>Link tidak ditemukan</p>
+      <p>Link not found</p>
     </div>
   );
 }
