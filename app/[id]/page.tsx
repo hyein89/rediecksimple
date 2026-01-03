b09d26b43195ce7d59084cce460cbf3633b792b6
hyein89 <bihontai@gmail.com>
2026-01-04 04:13:55 +0700

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -3,14 +3,21 @@ import links from "@/data/links.json";
 import { notFound } from "next/navigation";
 import RedirectLogic from "@/components/RedirectLogic";
 
-export default function Page({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const valid = links.find((x) => x.id === params.id);
+export async function generateMetadata({ params }: any) {
+  const data = (links as any)[params.id];
+  if (!data) return {};
 
-  if (!valid) notFound();
+  return {
+    title: data.title,
+    openGraph: {
+      images: [data.image],
+    },
+  };
+}
+
+export default function RedirectPage({ params }: any) {
+  const data = (links as any)[params.id];
+  if (!data) notFound();
 
   return <RedirectLogic />;
 }
