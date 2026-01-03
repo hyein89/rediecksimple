11d57ab8c4b8ae83118bca6656eef6cde486109a
hyein89 <bihontai@gmail.com>
2026-01-04 05:01:10 +0700

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,23 +1,50 @@
 
-import links from "@/data/links.json";
 import { notFound } from "next/navigation";
+import links from "@/data/links.json";
 import RedirectLogic from "@/components/RedirectLogic";
 
-export async function generateMetadata({ params }: any) {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+// META DINAMIS (TANPA DESCRIPTION)
+export async function generateMetadata({ params }: Props) {
   const data = (links as any)[params.id];
-  if (!data) return {};
+
+  if (!data) {
+    return {
+      title: "Not Found",
+    };
+  }
 
   return {
     title: data.title,
+    icons: {
+      icon: "/favicon.png",
+    },
     openGraph: {
-      images: [data.image],
+      title: data.title,
+      images: [
+        {
+          url: data.image,
+          width: 1200,
+          height: 630,
+        },
+      ],
+      type: "website",
     },
   };
 }
 
-export default function RedirectPage({ params }: any) {
+// PAGE
+export default function RedirectPage({ params }: Props) {
   const data = (links as any)[params.id];
-  if (!data) notFound();
+
+  if (!data) {
+    notFound();
+  }
 
   return <RedirectLogic />;
 }
